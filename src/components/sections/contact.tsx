c7963c8c1767da43ac6813c7ad72484917f81b77
Pushpa Sandhu <pushpashandhudevendrasing45@gmail.com>
2025-09-30 14:35:26 +0000

    the map still not pointing towards my business and a random area is been

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -54,7 +54,7 @@ const Contact = () => {
           <div className="flex flex-col gap-4">
             <div className="rounded-lg overflow-hidden shadow-lg h-64 md:h-80 lg:h-96">
                 <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3780.459388969145!2d73.76686947594952!3d18.6430962646698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b9c7682f1943%3A0x6b45a7095c27f670!2sShree%20Om%20Annapurna%20Caterers!5e0!3m2!1sen!2sin!4v1700030612345!5m2!1sen!2sin"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3780.4594519573404!2d73.7642944751939!3d18.6430931824795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b9c7682f1943%3A0x6b45a7095c27f670!2sShree%20Om%20Annapurna%20Caterers!5e0!3m2!1sen!2sin!4v1727589255893!5m2!1sen!2sin"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
