305b8a6abea99b5ea75da5c9d9d90791e5e7e137
Pushpa Sandhu <pushpashandhudevendrasing45@gmail.com>
2025-10-16 05:51:57 +0000

    i want to upload images and that image loading method only so guide me h

--- next.config.ts
+++ next.config.ts
@@ -25,12 +25,6 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'i.ibb.co',
         port: '',
         pathname: '/**',
